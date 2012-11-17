981114a2a98b48e780af647ac3db74672effff8e
Paul R Alexander <palexander@stanford.edu>
2012-11-16 17:23:28 -0800

    Basic tests for serializer

---
+++ lib/linked_data_serializer/test/test_request_formats.rb
@@ -0,0 +1,69 @@
+require "test/unit"
+require "rack/test"
+require "json"
+require_relative "../linked_data_serializer"
+require_relative "../lib/monkeypatches/to_flex_hash/object.rb"
+
+
+class TestLinkedDataSerializer < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    eval "Rack::Builder.new {( " + File.read(File.dirname(__FILE__) + '/test.ru') + "\n )}"
+  end
+
+  def test_request_accept_header
+    # Default is JSON
+    req
+    assert last_response.header["Content-Type"].eql?("application/json;charset=utf-8")
+
+    # Try html
+    req("text/html")
+    assert last_request.env["HTTP_ACCEPT"].eql?("text/html")
+    assert last_response.header["Content-Type"].eql?("text/html;charset=utf-8")
+
+    # Alright, xml?
+    req("application/rdf+xml")
+    assert last_request.env["HTTP_ACCEPT"].eql?("application/rdf+xml")
+    assert last_response.header["Content-Type"].eql?("application/rdf+xml;charset=utf-8")
+
+    # JSONP?
+    req("application/javascript")
+    assert last_request.env["HTTP_ACCEPT"].eql?("application/javascript")
+    assert last_response.header["Content-Type"].eql?("application/javascript;charset=utf-8")
+
+    # Turtle, beautiful Turtle
+    req("application/turtle")
+    assert last_request.env["HTTP_ACCEPT"].eql?("application/turtle")
+    assert last_response.header["Content-Type"].eql?("application/rdf+turtle;charset=utf-8")
+  end
+
+  def test_request_format_param
+    # GO JSON
+    req("", :format => "json")
+    assert last_response.header["Content-Type"].eql?("application/json;charset=utf-8")
+
+    # Try html
+    req("", :format => "html")
+    assert last_response.header["Content-Type"].eql?("text/html;charset=utf-8")
+
+    # Alright, xml?
+    req("", :format => "xml")
+    assert last_response.header["Content-Type"].eql?("application/rdf+xml;charset=utf-8")
+
+    # JSONP?
+    req("", :format => "jsonp")
+    assert last_response.header["Content-Type"].eql?("application/javascript;charset=utf-8")
+
+    # Turtle, beautiful Turtle
+    req("", :format => "turtle")
+    assert last_response.header["Content-Type"].eql?("application/rdf+turtle;charset=utf-8")
+  end
+
+  private
+
+  def req(accept = nil, params = {})
+    header "Accept", accept unless accept.nil?
+    get "/", params
+  end
+end
---
+++ lib/linked_data_serializer/test/test_serializer_output.rb
@@ -0,0 +1,53 @@
+require "test/unit"
+require "rack/test"
+require "json"
+require_relative "../linked_data_serializer"
+require_relative "../lib/monkeypatches/to_flex_hash/object.rb"
+
+class Person
+  def initialize(name, age, height = 6)
+    @name = name
+    @age = age
+    @height = height
+  end
+
+  def person_is_how_old
+    "#{@name} is #{@age}"
+  end
+
+  def name_upcase
+    @name.upcase
+  end
+
+  def relative_age
+    if @age < 10
+      "young"
+    elsif @age < 20
+      "teenager"
+    elsif @age > 20
+      "old"
+    end
+  end
+
+  def serializable_methods
+    [:relative_age, :name_upcase, :person_is_how_old]
+  end
+
+end
+
+class TestSerializerOutput < Test::Unit::TestCase
+  PERSON = Person.new("Simon", 21)
+
+  def test_json
+    json = LinkedData::Serializers.serialize(PERSON, :json)
+    reference = '{"name":"Simon","age":21,"height":6}'
+    assert_equal reference, json
+  end
+
+  def test_json_with_options
+    json = LinkedData::Serializers.serialize(PERSON, :json, :only => [:name])
+    reference = '{"name":"Simon"}'
+    assert_equal reference, json
+  end
+
+end
