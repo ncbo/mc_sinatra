bd2111a5859578751b609c6249bd5399fb1f6f5e
Paul R Alexander <palexander@stanford.edu>
2012-11-29 18:34:06 -0800

    Review tests

---
+++ lib/ontologies_linked_data/test/models/test_review.rb
@@ -0,0 +1,40 @@
+require_relative "../../ontologies_linked_data"
+require "test/unit"
+require "pry"
+
+class TestReview < Test::Unit::TestCase
+  def test_valid_review
+    r = LinkedData::Models::Review.new
+    assert (not r.valid?)
+
+    # Not valid because not all attributes are present
+    r.creator = "paul"
+    r.created = DateTime.parse("2012-10-04T07:00:00.000Z")
+    assert (not r.valid?)
+
+    # Still not valid because not all attributes are typed properly
+    r.body = "This is a test review"
+    r.ontologyReviewed = "SNOMED"
+    assert (not r.valid?)
+
+    # Fix typing
+    r.creator = LinkedData::Models::User.new(username: "paul")
+    r.ontologyReviewed = LinkedData::Models::Ontology.new(acronym: "SNOMED", name: "SNOMED CT")
+    assert r.valid?
+  end
+
+  def test_review_save
+    r = LinkedData::Models::Review.new({
+        :creator => LinkedData::Models::User.new(username: "paul"),
+        :created => DateTime.parse("2012-10-04T07:00:00.000Z"),
+        :body => "This is a test review",
+        :ontologyReviewed => LinkedData::Models::Ontology.new(acronym: "SNOMED", name: "SNOMED CT")
+      })
+
+    assert_equal false, r.exist?(reload=true)
+    r.save
+    assert_equal true, r.exist?(reload=true)
+    r.delete
+    assert_equal false, r.exist?(reload=true)
+  end
+end
