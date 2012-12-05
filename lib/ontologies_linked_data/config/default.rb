50704a34e24a9c45f5a22cc9fc4e56689a707d5f
Paul R Alexander <palexander@stanford.edu>
2012-12-05 11:19:01 -0800

    Use default settings when doing config

--- lib/ontologies_linked_data/config/default.rb
+++ lib/ontologies_linked_data/config/default.rb
@@ -1,5 +1,5 @@
 GOO_PORT = 9000
 GOO_HOST = "localhost"
 
-# Settings in this file can be overridden in the custom.rb
+# Settings in this file can be overridden in the custom.rb (warnings may happen)
 require_relative "custom.rb"
--- lib/ontologies_linked_data/ontologies_linked_data.rb
+++ lib/ontologies_linked_data/ontologies_linked_data.rb
@@ -9,8 +9,8 @@ Dir.glob(project_root + '/lib/models/*', &method(:require))
 # Setup Goo (repo connection and namespaces)
 module LinkedData
   def self.config(options = {})
-    port = options[:port] || 9000
-    host = options[:host] || "localhost"
+    port = options[:port] || GOO_PORT || 9000
+    host = options[:host] || GOO_HOST || "localhost"
     begin
       Goo.configure do |conf|
         conf[:stores] = [ { :name => :main , :host => host, :port => port, :options => { } } ]
