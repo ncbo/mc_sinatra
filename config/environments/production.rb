cb4f086a211db07aee42f38deb5941d69f41a151
Paul R Alexander <palexander@stanford.edu>
2012-12-05 15:28:35 -0800

    Only require if exists

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
 # Config Goo connection for LinkedData (repo connection and namespaces)
 # LinkedData.config
 
-require_relative "local/development.rb"
+require_relative "local/development.rb" if File.exist?("local/production.rb")
