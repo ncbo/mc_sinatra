bd400e29515b509d41206038a8293456f3e0ddbf
Paul R Alexander <palexander@stanford.edu>
2012-12-05 15:32:33 -0800

    Only require if file exists

--- lib/ontologies_linked_data/config/default.rb
+++ lib/ontologies_linked_data/config/default.rb
@@ -2,4 +2,4 @@ GOO_PORT = 9000
 GOO_HOST = "localhost"
 
 # Settings in this file can be overridden in the custom.rb (warnings may happen)
-require_relative "custom.rb"
+require_relative "custom.rb" if File.exist?("custom.rb")
