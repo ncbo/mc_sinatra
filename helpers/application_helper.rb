e6b372c4783ae21ba815df8aa568a3bec29103c8
Paul R Alexander <palexander@stanford.edu>
2012-12-12 07:05:15 -0800

    Helpers to initialize objects with attributes hash

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -6,6 +6,22 @@ module Sinatra
       def h(text)
         Rack::Utils.escape_html(text)
       end
+
+      # TODO: This will allow for mass-assignment problems and will allow
+      # users to overwrite any attribute, including things like passwords.
+      # TODO: This should use a Goo helper method to lookup attribute objects
+      # when the exist, for example when you need an IRI or a User object as a value.
+      def populate_from_params(obj, params)
+        params.each do |attribute, value|
+          obj.send("#{attribute}=", value) # if obj.respond_to?("#{attribute}=")
+        end
+        obj
+      end
+
+      def instance_from_params(cls, params)
+        n = cls.new
+        populate_from_params(n, params)
+      end
     end
 
     helpers ApplicationHelper
